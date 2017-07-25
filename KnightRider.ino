f896574dfe95a0cb9fe9772871f091c2dcb2e3a2
rchatterjee188 <rchatterjee188@gmail.com>
2017-07-25 15:50:25 -0400

    Add files via upload

---
+++ KnightRider.ino
@@ -0,0 +1,28 @@
+void setup() {
+  for (int i = 2; i<=8 ; i++){
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 2; i<=8; i++) {
+    for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
+      analogWrite(i, fadeValue);
+      delay(30);
+    }
+    for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) { 
+      analogWrite(i, fadeValue);
+      delay(30);
+    } 
+  }
+  for (int i = 7; i>=3; i--) {
+    for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
+      analogWrite(i, fadeValue);
+      delay(30);
+    }
+    for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) { 
+      analogWrite(i, fadeValue);
+      delay(30);
+    } 
+  }
+}
